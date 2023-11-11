597fbde86d2d49a9976678c6fd9cdf03e7827014
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-11 14:18:50 +0000

    TestToken added functions

--- TestToken.sol
+++ TestToken.sol
@@ -3,5 +3,24 @@
 pragma solidity 0.7.0;
 
 contract TestToken {
-    
+
+    address public minter;
+  mapping(address => uint) public getBalance;
+  uint public totalSupply;
+
+  constructor() public {
+    minter = msg.sender;
+  }
+
+  function mint(address _to, uint _amount) external {
+    require(msg.sender == minter, "not minter");
+    getBalance[_to] += _amount;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(getBalance[msg.sender] - _value >= 0);
+    getBalance[msg.sender] -= _value;
+    getBalance[_to] += _value;
+    return true;
+  }
 }
